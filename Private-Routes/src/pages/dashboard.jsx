4d1bd6ea7b9acd76da23f374bd0fcf9270a94c9b
asadishaq15 <asadishaq392@gmail.com>
2024-03-03 13:54:09 +0500

    add content to dashboard Page

--- Private-Routes/src/pages/dashboard.jsx
+++ Private-Routes/src/pages/dashboard.jsx
@@ -4,7 +4,9 @@ const Dashboard = () => {
   return (
     <div className="dashboard-container">
 
-      <p className="dashboard-text">Welcome to the dashboard!</p>
+      <p className="dashboard-text">Welcome to the <strong>dashboard!</strong></p>
+      <br/>
+      <p className="dashboard-text">This Page should be Protected by <strong>Private Route</strong></p>
     </div>
   );
 };
